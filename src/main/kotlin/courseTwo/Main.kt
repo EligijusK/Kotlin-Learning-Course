e6ad55c61846a838f8d789b1266e90101c55af56
EligijusK <e.kiudys@gmail.com>
2026-06-18 03:24:25 +0300

    Interfaces class finished

--- src/main/kotlin/courseTwo/Main.kt
+++ src/main/kotlin/courseTwo/Main.kt
@@ -1,8 +1,8 @@
 package org.example.courseTwo
 
 fun main(args: Array<String>) {
-    classesLesson(args)
-
+//    classesLesson(args)
+    interfacesLesson(args)
 }
 
 /* First Object-Oriented lesson working with classes Begins */
@@ -15,3 +15,34 @@ fun classesLesson(args: Array<String>) {
 }
 
 /* First Object-Oriented lesson working with classes Ends */
+
+/* Second Object-Oriented lesson working with interfaces Begins */
+
+interface StringProvider {
+    val placeHolder: String // Can't initialize property in interface, but it can be overrided in extended interfaces
+    fun getString(id: Int): String = "Id: $id"
+}
+
+interface ResourceProvider: StringProvider { // Extending interface
+    fun getDimension(id: Int): Long = id.toLong() // Default implementation
+}
+
+class DefaultResourceProvider: ResourceProvider { // All of implemented interface functions must be implemented
+
+    override val placeHolder = "<placeholder>"
+    // Resource provider doesn't need to implement other interface function since that interface have implementation of function
+}
+
+fun interfacesLesson(args: Array<String>) {
+    val someVariable: Any? = null
+    if (someVariable is StringProvider) {
+        println("The variable was StringProvider")
+    }
+
+    var stringProvider: DefaultResourceProvider = DefaultResourceProvider() // Created interface variable with assigned implemented interface DefaultResourceProvider
+    println(stringProvider.getString(0))
+    println(stringProvider.getDimension(1))
+    println(stringProvider.placeHolder)
+}
+
+/* Second Object-Oriented lesson working with interfaces Ends */
