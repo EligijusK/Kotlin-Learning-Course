2ce45ac0fd8ae7e626640082b76ae8d3ed878754
EligijusK <e.kiudys@gmail.com>
2026-06-18 04:34:31 +0300

    Data classes lesson finished

--- src/main/kotlin/courseTwo/Main.kt
+++ src/main/kotlin/courseTwo/Main.kt
@@ -3,7 +3,8 @@ package org.example.courseTwo
 fun main(args: Array<String>) {
 //    classesLesson(args)
 //    interfacesLesson(args)
-    enumsLesson(args)
+//    enumsLesson(args)
+    dataClassesLesson(args)
 }
 
 /* First Object-Oriented lesson working with classes Begins */
@@ -50,11 +51,6 @@ fun interfacesLesson(args: Array<String>) {
 
 /* Third Object-Oriented lesson working with Enums Begins */
 
-// North
-// East
-// South
-// West
-
 fun enumsLesson(args: Array<String>) {
     val direction:Direction = Direction.WEST // Created Enum that holds selected value
     println(direction.name) // Name value is string representation of selected value
@@ -70,3 +66,35 @@ fun enumsLesson(args: Array<String>) {
 }
 
 /* Third Object-Oriented lesson working with Enums Ends */
+
+/* Fourth Object-Oriented lesson working with Data classes Begins */
+
+fun dataClassesLesson(args: Array<String>) {
+    val task1 = Task("a1", "Add analytics") // Org task name: Build search feature
+    val task2 = Task("b2", "Add analytics")
+    println(task1.name)
+    println(task2.name)
+
+    val task2Copy = task2.copy() // Creates copy of data class automatically with variables
+    val (id, name) = task1 // Destructuring makes exactly the same as task1 values, if only two values are used for it, it will take only those two values
+
+    if (task1.name == task2.name) { // Comparison of two names
+        println("Tasks are the same")
+    } else {
+        println("Tasks are different")
+    }
+
+    if (task1 == task2) { // Compiler handles comparison of data class constructor variables, so there are no need to compare manually
+        println("Tasks are the same")
+    } else {
+        println("Tasks are different")
+    }
+
+    if (task2Copy == task2) { // Compiler handles comparison of data class constructor variables, so there are no need to compare manually
+        println("Tasks are the same")
+    } else {
+        println("Tasks are different")
+    }
+}
+
+/* Fourth Object-Oriented lesson working with Data classes Ends */
---
+++ src/main/kotlin/courseTwo/Task.kt
@@ -0,0 +1,5 @@
+package org.example.courseTwo
+
+data class Task(val id: String, val name: String) { // Data class modifier, it must contain at least on constructor property
+
+}
