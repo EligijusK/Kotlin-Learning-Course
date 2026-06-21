4a15ecdbc92319d75886d9191a3acbb6bd03aa7e
EligijusK <e.kiudys@gmail.com>
2026-06-22 01:17:02 +0300

    Use modifiers lesson finished

--- app/src/main/java/com/example/sample/0106Modifiers.kt
+++ app/src/main/java/com/example/sample/0106Modifiers.kt
@@ -1,9 +1,11 @@
 package com.example.sample
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -13,6 +15,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,13 +34,17 @@ fun Modifiers0106(modifier: Modifier = Modifier) {
                 Box(
                     modifier = Modifier
                         .height(50.dp)
+                        .padding(top = 4.dp, end = 4.dp) // Is for add padding to selected sides
+                        .weight(1f) // Is used for splitting in row elements
                         .background(MaterialTheme.colorScheme.secondary)
                 )
                 Box(
                     modifier = Modifier
                         .height(50.dp)
                         .padding(top = 4.dp)
+                        .weight(1f)
                         .background(MaterialTheme.colorScheme.tertiary)
+                        .clickable(onClick = {}) // To make box clickable
                 )
                 Box(
                     modifier = Modifier
@@ -50,10 +57,13 @@ fun Modifiers0106(modifier: Modifier = Modifier) {
                         modifier = Modifier
                             .width(24.dp)
                             .height(24.dp)
+                            .align(Alignment.TopCenter)
                             .background(MaterialTheme.colorScheme.background)
                     )
                 }
             }
+
+            Spacer(modifier = Modifier.height(16.dp)) // This adds spacer between
 
             Box(
                 modifier = Modifier
@@ -61,6 +71,8 @@ fun Modifiers0106(modifier: Modifier = Modifier) {
                     .height(200.dp)
                     .background(MaterialTheme.colorScheme.error)
             )
+
+            Spacer(modifier = Modifier.height(16.dp))
 
             Box(
                 modifier = Modifier
