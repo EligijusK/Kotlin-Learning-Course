84ebc59901d7d2e84792dc2e4bc9699795193435
EligijusK <e.kiudys@gmail.com>
2026-06-23 00:47:51 +0300

    Use the remember function lesson finished

--- app/src/main/java/com/example/sample/0304Remember.kt
+++ app/src/main/java/com/example/sample/0304Remember.kt
@@ -34,11 +34,11 @@ private fun Remember0304(
             alignment = Alignment.CenterVertically
         )
     ) {
-        var numAttendees by remember { mutableIntStateOf(0) }
+        var numAttendees = mutableIntStateOf(0) // It doesn't remember state now recompose creates new variable every time when recomposition happens
         Tag(
             tag = "Android Basics",
-            numAttendees = numAttendees,
-            onTagClick = { numAttendees++ }
+            numAttendees = numAttendees.intValue,
+            onTagClick = { numAttendees.intValue++ }
         )
 
         var numDoubleAttendees by remember { mutableIntStateOf(1) }
