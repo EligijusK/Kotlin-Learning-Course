c2c3bbcb84763fc5372859ef0bc671ee804e551d
EligijusK <e.kiudys@gmail.com>
2026-06-24 12:29:25 +0300

    Intorduction to unidirectional data flow and Dependency injection with koin lessons finished

--- app/src/main/java/com/example/sample/0402Koin.kt
+++ app/src/main/java/com/example/sample/0402Koin.kt
@@ -3,14 +3,24 @@ package com.example.sample
 import androidx.lifecycle.ViewModel
 
 // our class that uses a dependency
-class SampleViewModel: ViewModel() {
-    val repository: DefaultSampleRepository = DefaultSampleRepository()
+class SampleViewModel(repository: SampleRepository): ViewModel() { // Using dependency injection
 
     init {
         repository.doSomething()
     }
 }
 
-class DefaultSampleRepository() {
+interface SampleRepository {
     fun doSomething() {}
 }
+
+class DefaultSampleRepository(): SampleRepository {
+    override fun doSomething() {}
+}
+
+class FakeSampleRepository(): SampleRepository {
+    override fun doSomething() {}
+}
+
+val viewModel = SampleViewModel(DefaultSampleRepository())
+val viewModelInTest = SampleViewModel(FakeSampleRepository())
